025ff1b1f5cfc6767e9b853759cca55b352a1882
beneko <behzadnekouei@gmail.com>
2021-08-23 15:26:13 +0200

    three method for images created

--- app/src/main/java/com/afpa/coffeeshop/MainActivity.java
+++ app/src/main/java/com/afpa/coffeeshop/MainActivity.java
@@ -8,19 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.afpa.coffeeshop.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -64,4 +59,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Affiche un message disant que le produit a été sélectionné
+     *
+     */
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+
+    }
+    public void showIceCreamOrder(View view) {
+        displayToast(getString(R.string.ice_cream_order_message));
+
+    }
+    public void showFroyoOrder(View view) {
+        displayToast(getString(R.string.froyo_order_message));
+
+    }
 }
